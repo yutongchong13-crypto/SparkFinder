33ba21c7b7a11a408677019eefd21d9c2e61369d
yutongchong13-crypto <yutong.chong13@rafflesgirlssch.edu.sg>
2026-07-23 12:38:59 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,6 @@
-// SparkFinder - Supabase Connection
-
 const SUPABASE_URL = "https://eewivekywdsmrwrgttgu.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_7y-0oLodPiWMH19sT0gpCw_qbA0fvtg";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const { createClient } = supabase;
+
+const db = createClient(SUPABASE_URL, SUPABASE_KEY);
